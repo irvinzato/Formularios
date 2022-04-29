095ad41183b6eda3bdc6dcf0de4de12e6e62c32c
irvinzato <multizato@hotmail.com>
2022-04-28 23:29:35 -0500

    Se trabajo toda la parte de formularios REACTIVOS, con algunos comentarios por que pueden servir para resolver algunos problemas, muy potentes utilizarlos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  //Nombre de mi fomulario inicializado, ahora debo pasarselo a mi HTML
+ /*  miFormulario: FormGroup = new FormGroup({
+    nombreProducto: new FormControl('RTX 4563'),   //Entre parentesis es el valor que tendra por defecto y es el mismo que debe tener el HTML
+    precio        : new FormControl(1500),
+    existencias   : new FormControl(5)
+  }) */
+
+
+
+  miFormulario: FormGroup = this.fb.group({
+    nombreProducto: [ , [ Validators.required, Validators.minLength(3) ] ],
+    precio        : [ , [ Validators.required, Validators.min(0) ] ],
+    existencias   : [ , [ Validators.required, Validators.min(0) ]]
+  })
+
+              //Puedo utilizar FormBuilder para formularios mas complejos, es practicamente lo mismo que "new FormGroup()" pero mas legible, queda a gusto de cada uno
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit() {
+    //Por si necesito inicializar los campos de mi formulario setValue, pero todos los valores del objeto deben venir
+    this.miFormulario.reset({  //Por eso mejor ocupo reset, aqui puedo poner los que quiera
+      nombreProducto: 'RTW ASDS',   
+      precio: 1200
+    })
+  }
+
+  campoNoValido(campo: string) {
+    return this.miFormulario.controls[campo].errors
+          && this.miFormulario.controls[campo].touched;
+  }
+
+  guardar() {
+    //Otra manera de ver si es invalido el formulario, o podemos poner el [disabled]="miFormulario.invalid" en el HTML
+    if(this.miFormulario.invalid){  
+      this.miFormulario.markAllAsTouched();   //Metodo para que toque todos los campos
+      return;
+    }
+    console.log("Mi Formulario value es ", this.miFormulario.value);
+    this.miFormulario.reset();        //Metodo para resetear todo el formulario
   }
 
 }
--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  //Formulario que utilizo en el HTML
+  miFormulario: FormGroup = this.fb.group({
+    genero: [ 'M', Validators.required ],
+    notificaciones: [ true, Validators.required ],
+    condiciones: [ false, Validators.requiredTrue ] //Como paso a ser null, Valido que afuerza sea true
+  });
+
+  //Objeto que tiene los datos que me interesan pasar o trabajar
+  persona = {
+    id: 123,
+    genero: 'F',
+    notificaciones: true
+  }
+
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit() {
+    //Cuando hago este reset los campos que no tenga mi formulario de persona los pasa como "null" por defecto, por lo que debemos tratar esas condiciones
+    //Tambien puedo pasarle los campos que no tiene mi objeto y establecer el valor que quiero
+    this.miFormulario.reset({
+      ...this.persona,
+      condiciones: false
+    }); 
+    //this.miFormulario.reset( this.persona );  //Establezco los valores de mi objeto en mi formulario, los que no estan se vuelven null
+
+    //PARA SABER INMEDIATAMENTE CUANDO CAMBIA SU VALOR
+    //this.miFormulario.get('condiciones').valueChanges.subscribe( res => console.log(res) );
+
+    //PARA SABER INMEDIATAMENTE QUE CAMBIA EL VALOR DEL FORMULARIO Y PASARLO AL OBJETO, NO ES MUY COMUN PERO PUEDE SERVIR
+    /* this.miFormulario.valueChanges.subscribe( res => {
+      delete res.condiciones;
+      this.persona = res;
+    }); */
+
   }
+
+  guardar() {
+
+    if(this.miFormulario.invalid){ return; }
+
+    const formValue = this.miFormulario.value;
+    delete formValue.condiciones;    //Podria borrar lo que no ocupe de mi formulario y pasarlo a mi objeto
+
+    this.persona = formValue;
+    //this.persona.genero = formValue.genero;   //Para mantener variables de mi objeto por que lo de arriba borra las variables que no tiene, como id
+    //this.persona.notificaciones = formValue.notificaciones;
+
+    console.log("formValue tiene ", formValue);
+    console.log("objeto persona tine ", this.persona);
+  }
+
 
 }
